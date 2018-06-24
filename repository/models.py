178ed0801942c27d0f98ff98915aadf995bb1cf7
Roberto Oropeza <roropeza@gcg-usa.net>
2018-06-24 15:15:06 -0400

    Add teams, match and betting models

--- repository/models.py
+++ repository/models.py
@@ -29,6 +29,46 @@ class User(db.Model, UserMixin):
     token = db.Column(db.String(200))
 
 
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code3 = db.Column(db.String(3), unique=True, nullable=False)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+    flag_picture = db.Column(db.String(200))
+
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    timezone = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200))
+    team1_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    team1 = relationship('Team', uselist=False, foreign_keys=[team1_id])
+    team2_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    team2 = relationship('Team', uselist=False, foreign_keys=[team2_id])
+    finished = db.Column(db.Boolean)
+    score1 = db.Column(db.Integer)
+    score2 = db.Column(db.Integer)
+
+
+class Bettable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    opened_since = db.Column(db.DateTime)
+    closed_from = db.Column(db.DateTime)
+    match_id = db.Column(db.Integer, db.ForeignKey('match.id'))
+    match = relationship('Match', uselist=False)
+
+
+class Bet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    bettable_id = db.Column(db.Integer, db.ForeignKey('bettable.id'))
+    bettable = relationship('Bettable', uselist=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = relationship('User', uselist=False)
+    score1 = db.Column(db.Integer)
+    score2 = db.Column(db.Integer)
+    submitted_on = db.Column(db.DateTime, nullable=False)
+
+
 def seed_data(app):
     print('Seeding database')
     identity = Identity(email='oropezaroberto@gmail.com')
